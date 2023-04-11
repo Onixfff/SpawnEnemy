7f4dd1e4ca14397199a3e55851da8212d9dc199c
Onixfff <danilnikonov01@mail.ru>
2023-04-11 12:16:46 +0200

    Создал новый скрипт Enemy.cs. Внес в него prefab enemy. В классе EnemySpawn ссылаюсь на Enemy для получения prefab. В EnemySpawn создал переменную _waitForSeconds и при вызове корутины SpawnerCorutine ссылаюсь на эту переменную.

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class EnemySpawn : MonoBehaviour
 {
     [SerializeField] private float _delaySpawn = 2f;
-    [SerializeField] private GameObject _enemy;
+    [SerializeField] private Enemy _enemy;
+    [SerializeField] private GameObject _enemyPrefab;
 
+    private WaitForSeconds _waitForSeconds;
     private Transform[] _spawnPoints;
 
     void Awake()
     {
+        _enemyPrefab = _enemy.GetEnemyPrefab();
+        _waitForSeconds = new WaitForSeconds(_delaySpawn);
         _spawnPoints = GetComponentsInChildren<Transform>();
         StartCoroutine(SpawnerCoroutine());
     }
@@ -19,8 +23,8 @@ public class EnemySpawn : MonoBehaviour
     {
         for (int i = 0; i < _spawnPoints.Length; i++)
         {
-            Instantiate(_enemy, _spawnPoints[i]);
-            yield return new WaitForSeconds(_delaySpawn);
+            Instantiate(_enemyPrefab, _spawnPoints[i]);
+            yield return _waitForSeconds;
         }
     }
 }
